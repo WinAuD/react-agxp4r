08801022a1e2f4bc7ed30e81cf28d50dfa678a38
WinAuD <mailchris1@gmx.de>
2021-03-22 21:17:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-agxp4r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-agxp4r)
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class MyNum extends React.Component {
+            render() {
+                const myNiceNum = {
+                    display: "inline-block",    // display: inline-block;
+                    padding: "30px",            // padding: 30px
+                    borderRadius: '50%',        // border-radius: 50%
+                    backgroundColor: this.props.myColor, // background-color: #C6DCEE
+                    fontFamily: this.props.myColor,      // font-family: fantasy
+                    fontSize: 64,           // font-size: 64px
+                    textAlign: "center"         // text-align: center
+                };
+
+                return (
+                    <div style={myNiceNum}>
+                        {this.props.children}
+                    </div>
+                );
+            }
+        }
+
+        ReactDOM.render(
+            <div>
+                <MyNum myColor="#AEA" myFont="courier">1</MyNum>
+                <MyNum myColor="#EEA" myFont="courier">3</MyNum>
+                <MyNum myColor="#AAA" myFont="courier">4</MyNum>
+                <MyNum myColor="#EEE" myFont="courier">5</MyNum>
+
+            </div>,
+            document.getElementById("myContainer")
+        );
