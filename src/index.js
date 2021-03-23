659c56af4d09bdd981de529030ca8928db7d1663
WinAuD <mailchris1@gmx.de>
2021-03-23 13:35:51 +0100

    1 file modified

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 class MyNum extends React.Component {
             render() {
                 const myNiceNum = {
-                    display: "inline-block",    // display: inline-block;
+                    display: "inline-block",    // display: inline-block TEST;
                     padding: "30px",            // padding: 30px
                     borderRadius: '50%',        // border-radius: 50%
                     backgroundColor: this.props.myColor, // background-color: #C6DCEE
