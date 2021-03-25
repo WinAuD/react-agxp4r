8a9f47876b1eac4795675ed233390f508ff24e21
WinAuD <mailchris1@gmx.de>
2021-03-25 12:50:27 +0100

    1 file modified, 2 added

---
+++ src/react01.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './react.css';
+
+ReactDOM.render(
+  
+    <div>Goreact</div>,
+  
+  document.getElementById("myContainer")
+);
